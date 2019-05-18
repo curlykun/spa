0fb10c43a283237a13302c08422f1f5697741d2d
curlykun <kurob1993@gmail.com>
2019-05-18 14:59:27 +0700

    Updates

---
+++ precache-manifest.2d7ce5ccf43201ece01271a84ac33a51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a2cd21a9f490822aa68fb3f21deda82",
+    "url": "/spa/index.html"
+  },
+  {
+    "revision": "c105c629150b2e293392",
+    "url": "/spa/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "35d62a769b70626670a8",
+    "url": "/spa/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "c105c629150b2e293392",
+    "url": "/spa/static/js/2.0895a7ef.chunk.js"
+  },
+  {
+    "revision": "35d62a769b70626670a8",
+    "url": "/spa/static/js/main.97ccaa54.chunk.js"
+  },
+  {
+    "revision": "f20ae6f6f970b023f341",
+    "url": "/spa/static/js/runtime~main.f20bd972.js"
+  }
+]);
